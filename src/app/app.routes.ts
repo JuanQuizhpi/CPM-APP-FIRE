f3b3116a33b707e0f85c779f505b3d7a45e52964
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2024-12-19 12:02:02 -0500

    Implementacion Guards al sitio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import HomeComponent from './pages/home/home.component';
 import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 import { LogInComponent } from './pages/auth/log-in/log-in.component';
+import { authGuard, publicGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: '', component:HomeComponent
+    path: '', component:HomeComponent,
+    canActivate:[authGuard]
   },
   {
     path: 'auth',
+    canActivate:[publicGuard],
     children: [
       {
         path: 'sign-up',component:SignUpComponent
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,38 @@ import { AuthService } from '../../core/services/auth.service';
 })
 export default class HomeComponent {
   private authService = inject(AuthService);
+  private _router = inject(Router);
+
   async logOut(): Promise<void> {
     try {
       await this.authService.logOut();
+      this._router.navigateByUrl('/auth/log-in');
     } catch (error) {
       console.log(error);
     }
   }
+
+
+
+  /**
+   * Control Log Out 
+   */
+
+  showLogoutConfirmation() {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Estás a punto de cerrar sesión.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, cerrar sesión',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.logOut();
+        //this.loginService.logout();
+      }
+    });
+  }
+
+
 }
