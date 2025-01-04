8efb7112d285a4d5b3dd40123be4a588c123c739
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2025-01-03 22:49:12 -0500

    Configuraciones Componente Libros Admin

--- src/app/pages/adminrole/libroadmin/libroadmin.component.ts
+++ src/app/pages/adminrole/libroadmin/libroadmin.component.ts
@@ -1,12 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService, Book } from '../../../core/services/books.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { error } from 'console';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import Swal from 'sweetalert2';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-libroadmin',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
   templateUrl: './libroadmin.component.html',
-  styleUrl: './libroadmin.component.scss'
+  styleUrl: './libroadmin.component.scss',
 })
-export class LibroadminComponent {
+export class LibroadminComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService) {}
+ 
+  /*
+  loadBooks(): void {
+    this.bookServie.getBooks().subscribe((books) => {
+      this.books = books;
+    });
+  }*/
+
+  ngOnInit(): void {
+    //this.loadBooks();
+    this.bookService.getBooks().subscribe((books) => {
+      this.dataSource.data = books;
+    });
+  }
+
+  displayedColums: string[] = [
+    'title',
+    'author',
+    'editorial',
+    'publishedYear',
+    'availability','actions'
+  ];
+  dataSource = new MatTableDataSource<Book>();
+  searchText: string = '';
+  searchField: string = 'title';
+
+  applyFilter(): void {
+    const normalizeSearch = this.normalizeText(this.searchText);
+    this.dataSource.filterPredicate = (data: Book, filter: string) => {
+      const field = data[this.searchField as keyof Book] as string | number;
+      return this.normalizeText(field?.toString() || '').includes(filter);
+    };
+    this.dataSource.filter = normalizeSearch;
+  }
+
+  private normalizeText(text: string): string {
+    return text
+      .toLocaleLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
 }
