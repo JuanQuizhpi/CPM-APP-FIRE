f6d43c1ec186ae057174c8d2830f20e633467385
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2025-01-03 20:02:31 -0500

    Control de opciones por rol

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule , MatIconButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { user } from '@angular/fire/auth';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule,MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule,MatButtonModule,NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })  
-export default class HomeComponent {
+export default class HomeComponent implements OnInit {
+
+  userRole: 'student' | 'admin' | null = null ;
+
   private authService = inject(AuthService);
   private _router = inject(Router);
 
@@ -24,6 +29,18 @@ export default class HomeComponent {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  ngOnInit(): void {
+    //Obtener el rol del usuario autenticado
+    this.authService.authState$.subscribe((user)=>{
+      if(user){
+        const email = user.email || '';
+        this.userRole =this.authService.getUserRole(email);
+      }else{
+        this.userRole = null;
+      }
+    });
   }
 
 
