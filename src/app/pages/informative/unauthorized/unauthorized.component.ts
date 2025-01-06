6ee1379efc83d13e6bccde3d1a14333ff9811a5b
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2025-01-06 00:21:52 -0500

    Configuracion componente de advertencia

--- src/app/pages/informative/unauthorized/unauthorized.component.ts
+++ src/app/pages/informative/unauthorized/unauthorized.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.scss'
 })
 export class UnauthorizedComponent {
+
+  constructor(private router: Router) {}
+
+  redirectToHome(): void {
+    this.router.navigate(['/']); 
+  }
 
 }
--- src/app/pages/presentacion/socialmedia/socialmedia.component.ts
+++ src/app/pages/presentacion/socialmedia/socialmedia.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-socialmedia',
   standalone: true,
