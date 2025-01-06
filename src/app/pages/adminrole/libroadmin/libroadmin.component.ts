63ecc1582f7bd273534feb60b8c31021435dfdcf
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2025-01-06 10:05:06 -0500

    Configuracion componente libros admin

--- src/app/pages/adminrole/libroadmin/libroadmin.component.ts
+++ src/app/pages/adminrole/libroadmin/libroadmin.component.ts
@@ -70,6 +70,7 @@ export class LibroadminComponent implements OnInit {
     'author',
     'editorial',
     'edicion',
+    'city',
     'publishedYear',
     'bibliografiaSGS',
     'availability',
