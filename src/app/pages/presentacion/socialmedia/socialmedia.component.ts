76b533eb713c94a59babdde3e852a299b97e3d32
JuanQuizhpi <140675909+JuanQuizhpi@users.noreply.github.com>
2025-01-06 00:10:15 -0500

    Configuracion Estilos componente Social Media

--- src/app/pages/presentacion/socialmedia/socialmedia.component.ts
+++ src/app/pages/presentacion/socialmedia/socialmedia.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-socialmedia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule,MatButtonModule,NgFor],
   templateUrl: './socialmedia.component.html',
-  styleUrl: './socialmedia.component.scss'
+  styleUrl: './socialmedia.component.scss',
 })
 export class SocialmediaComponent {
+  socialMedia = [
+    {
+      name: 'Instagram',
+      description: 'Síguenos en Instagram para ver fotos y actualizaciones.',
+      link: 'https://www.instagram.com/ciudadpatrimoniomundial/',
+    },
+    {
+      name: 'YouTube',
+      description: 'Visita nuestro canal de YouTube para contenido en video.',
+      link: 'https://www.youtube.com/channel/UCwdISLeiPE5o7pKUKlqlKNg',
+    },
+    {
+      name: 'Facebook',
+      description: 'Conéctate con nosotros en Facebook.',
+      link: 'https://www.facebook.com/proyectoCPM',
+    },
+  ];
 
+  openLink(link: string): void {
+    window.open(link, '_blank');
+  }
 }
